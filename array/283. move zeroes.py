72c2790761dd90075895947039d1e08c30020e58
Gurjot Singh <gurjotgagan@gmail.com>
2025-01-21 19:36:22 +0530

    changes
    changes

--- array/283. move zeroes.py
+++ array/283. move zeroes.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         l = 0
@@ -5,3 +7,8 @@ class Solution:
             if value != 0:
                 nums[l], nums[current] = nums[current], nums[l]
                 l += 1
+
+nums = [0, 1, 0, 3, 12]
+solution = Solution()
+solution.moveZeroes(nums)
+print(nums)
